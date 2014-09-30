3c9901daafdbe8137722ee36dc1184bf73997ea1
Sean Dwyer <ewetoo@gmail.com>
2014-09-30 22:09:03 +1000

    initial revision control

---
+++ index.php
@@ -0,0 +1,40 @@
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<!doctype html>
+<html>
+<head>
+<link rel=icon href="favicon.ico">
+<link rel=stylesheet href="default.css" type=text/css>
+<script src="js/modernizr-latest.js"></script>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<title>ewe2.ninja</title>
+</head>
+<body>
+<div class=header>
+<a class=headline href="/">ewe2.ninja</a>
+</div>
+<div class=menu>
+<div class=mainicon><img alt="ninja penguin" src="ewe2.ninja.png"><br/>
+<script language="JavaScript">
+var Q=new Array();
+Q[0] = "eek";
+Q[1] = "eek eek";
+Q[2] = "EEK eek FISH";
+Q[3] = "There is no utopia. There never will be";
+Q[4] = "Give a penguin a fish and he'll go away. Give him ten fish and you've got yourself a penguin party!";
+Q[5] = "The past is gone and the future is imaginary. Be here now.";
+document.write(Q[Math.floor(Math.random() * Q.length)]);
+</script>
+</div>
+</div>
+<div class=footer>
+<div class=hosting>
+<span class=hosting1>Hosting by <a href=http://cloudatcost.com>Cloudatcost.com</a></span>
+<span class=hosting2>Discount coupon: <strong>nunEny6AsE</strong> expires 2014-10-03!</span>
+</div>
+<?php
+echo 'Date: '. date('Y-m-d') ."\n";
+?>
+</div>
+</body></html>
