7e56f64f3865274d5904b2273ff453fc58719183
Sean Dwyer <ewetoo@gmail.com>
2014-12-03 06:11:20 +1100

    updates for fonts

--- index.php
+++ index.php
@@ -76,6 +76,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/webfont.js"></script>
+    <script src="js/dofont.js"></script>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/modernizr-latest.js"></script>
