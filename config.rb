d7587c63590a91f2e141d999fe3095ee43823f91
Sean Dwyer <ewetoo@gmail.com>
2014-12-28 03:10:55 +1100

    fiddling with some css setting

--- config.rb
+++ config.rb
@@ -66,13 +66,13 @@ set :markdown_engine, :kramdown
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :cache_buster
 
   # Use relative URLs
   # activate :relative_assets
