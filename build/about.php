e0f2bfd5d7b6a775354848c4a54ae9659829541e
Sean Dwyer <ewetoo@gmail.com>
2014-12-28 00:47:38 +1100

    added build dir for master so we can version and merge properly

---
+++ build/about.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <meta content='IE=edge' http-equiv='X-UA-Compatible'>
+    <meta content='width=device-width, initial-scale=1' name='viewport'>
+    <meta content='penguin home' name='description'>
+    <meta content='ewe2' name='author'>
+    <link href='favicon.ico' rel='icon'>
+    <title>ewe2.ninja</title>
+    <!-- Bootstrap core CSS -->
+    <link href='css/bootstrap.min.css' rel='stylesheet'>
+    <!-- Custom styles for this template -->
+    <link href='css/screen.css' media='screen' rel='stylesheet'>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class='site-wrapper'>
+      <div class='site-wrapper-inner'>
+        <div class='cover-container'>
+          <div class='masthead clearfix'>
+            <div class='inner'>
+              <h1 class='masthead-brand'>ewe2.ninja</h1>
+              <ul class='nav masthead-nav'>
+                <li>
+                  <a href='/'>home</a>
+                </li>
+                <li>
+                  <a href='stuff.php'>stuff</a>
+                </li>
+                <li class='active'>
+                  <a href='about.php'>about</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class='inner cover'>
+            <h2 class='cover-heading'>About ewe2.ninja</h2>
+            <h3 class='lead'>The homesite of Sean Dwyer on the big wide internets.</h3>
+            <p class='lead'>
+              <a class='btn btn-lg btn-default' href='#'>Under Construction</a>
+            </p>
+          </div>
+          <div class='mastfoot'>
+            <div class='mastfoot-inner'>
+              <span class='bumf1'>
+                Hosting by
+                <a href='http://cloudatcost.com'>Cloudatcost.com</a>
+              </span>
+              <span class='boiler bumf2'>
+                <?php echo 'Modified: '. date('Y-m-d') ."\n"; ?>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--
+      Bootstrap core JavaScript
+      ==================================================
+    -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src='js/jquery.min.js'></script>
+    <script src='js/bootstrap.min.js'></script>
+    <script src='js/modernizr-latest.js'></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src='js/ie10-viewport-bug-workaround.js'></script>
+  </body>
+</html>
